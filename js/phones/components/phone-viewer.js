ab642f1ec0e8f618bf3d1f6eeb0aef207f7d1f36
MAKSYM_enko <finside41@gmail.com>
2019-01-30 21:54:15 +0200

    Gallery was implemented //

--- js/phones/components/phone-viewer.js
+++ js/phones/components/phone-viewer.js
@@ -8,6 +8,12 @@ export default class PhoneViewer extends Component {
     this._onBack = onBack;
 
     this.on('click', 'back-button', this._onBack);
+    this.on('click', 'small-image', (event) => {
+      const smallImage = event.target;
+      const largeImage = this._element.querySelector('[data-element="large-image"]');
+
+      largeImage.src = smallImage.src;
+    });
   }
 
   show(phoneDetails) {
@@ -18,8 +24,13 @@ export default class PhoneViewer extends Component {
   }
   _render() {
     const phone = this._phoneDetails;
+
     this._element.innerHTML = `
-      <img class="phone" src="${ phone.images[0] }">
+        <img
+        data-element="large-image"
+        class="phone"
+        src="${ phone.images[0] }"
+        >
       <button data-element="back-button">
         Back
       </button>
@@ -29,28 +40,20 @@ export default class PhoneViewer extends Component {
   
       <h1>${ phone.name }</h1>
   
-      <p>Motorola XOOM with Wi-Fi has a super-powerful dual-core processor and Android™ 3.0 (Honeycomb) — the Android platform designed specifically for tablets. With its 10.1-inch HD widescreen display, you’ll enjoy HD video in a thin, light, powerful and upgradeable tablet.</p>
+      
+  
+      <p>${ phone.description }</p>
   
       <ul class="phone-thumbs">
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.1.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.2.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.3.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.4.jpg">
-        </li>
-        <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.5.jpg">
-        </li>
-      </ul>
-    `;
-  }
+      ${ phone.images.map(imageUrl => `
+      <li>
+        <img
+          data-element="small-image"
+          src="${ imageUrl }"
+        >                
+      </li>
+    `).join('') }
+  </ul>
+`;
+}
 }
