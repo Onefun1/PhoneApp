968ac6fbe99fc23d3913add12871d00223f63f1b
MAKSYM_enko <finside41@gmail.com>
2019-01-30 20:21:22 +0200

    Details opened from Image or Name

--- js/phones/components/phone-catalog.js
+++ js/phones/components/phone-catalog.js
@@ -16,11 +16,13 @@ export default class PhoneCatalog extends Component {
         this._render();
     
         this._element.addEventListener('click', (event) => {
-          let phoneElement = event.target.closest('[data-element="phone"]');
+          let detailsLink = event.target.closest('[data-element="details-link"]');
     
-          if (!phoneElement) {
+          if (!detailsLink) {
             return;
           }
+
+          let phoneElement = detailsLink.closest('[data-element="phone"]');
     
           this._onPhoneSelected(phoneElement.dataset.phoneId);
         });
@@ -32,9 +34,20 @@ export default class PhoneCatalog extends Component {
         <ul class="phones">
         ${ this._phones.map(phone => `
         
-          <li class="thumbnail" data-element="phone" data-phone-id="${ phone.id }">
-            <a href="#!/phones/${ phone.id}" class="thumb">
-              <img alt="${ phone.name}" src="${ phone.imageUrl}">
+        <li
+        data-element="phone"
+        data-phone-id="${ phone.id }"
+        class="thumbnail"
+      >
+        <a
+          data-element="details-link"
+          href="#!/phones/${ phone.id }"
+          class="thumb"
+        >
+          <img
+            alt="${ phone.name }"
+            src="${ phone.imageUrl }"
+          >
             </a>
             <div class="phones__btn-buy-wrapper">
             <a class="btn btn-success">
@@ -42,7 +55,12 @@ export default class PhoneCatalog extends Component {
             </a>
           </div>
 
-          <a href="#!/phones/motorola-xoom-with-wi-fi">${ phone.name}</a>
+          <a
+              data-element="details-link"
+              href="#!/phones/motorola-xoom-with-wi-fi"
+            >
+              ${ phone.name }
+            </a>
           <p>${ phone.snippet}</p>
         </li>
       
